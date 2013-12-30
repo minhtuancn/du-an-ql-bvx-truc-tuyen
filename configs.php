16bb664cdd735c91ec0e6eb73dfb49a57f928d1b
thanhctk34@gmail.com <thanhctk34@gmail.com@d0e9053a-3570-3f8e-9e5b-0b7ea4c43745>
2013-12-30 12:34:33 +0000

    tao configs.php (ket noi co so du lieu)

---
+++ configs.php
@@ -0,0 +1,15 @@
+﻿<?php
+	// ket noi toi server
+	function ConnectStatic(){
+		$conn = mysql_connect("localhost","root","") or die("Loi ket noi !");
+		mysql_query("SET NAMES 'UTF8'",$conn);
+		mysql_select_db("webvexe",$conn);
+		return $conn;
+	}	
+	// truy van du lieu
+	function Query($qString){
+		$conn = ConnectStatic();
+		$kq = mysql_query($qString,$conn);
+		return $kq;
+	}	
+?>
